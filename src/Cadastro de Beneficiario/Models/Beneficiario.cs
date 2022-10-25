5a7c6ba27b6162e75bd2fc82681b65838d032b1f
dridrileite <adrianacristinasouza@gmail.com>
2022-10-25 11:27:02 -0300

    feat: Adicionar o model Beneficiario

---
+++ src/Cadastro de Beneficiario/Models/Beneficiario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("Cadastro de Beneficiário")]
+public class Beneficiario
+{
+	[Key]
+	public int Id_Beneficiario { get; set; }
+	[Required(ErrorMessage ="Campo obrigatório!")]
+	public string CPF { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório!")]
+    public string Nome_Beneficiario { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório!")]
+    public string Situacao { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório!")]
+    public DateOnly Data_Nascimento { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório!")]
+    public string CEP { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório!")]
+    public string Logradouro { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório!")]
+    public string Numero { get; set; }
+
+	public string Complemento { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório!")]
+    public string Cidade { get; set; }
+
+    [Required(ErrorMessage = "Campo obrigatório!")]
+    public string UF { get; set; }
+
+    
+}
---
+++ src/Cadastro de Beneficiario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cadastro_de_Beneficiario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
