9522184acaf404f86aa7463e27e43756c5191e47
dridrileite <adrianacristinasouza@gmail.com>
2022-10-25 15:06:12 -0300

    feat: adicicão controller cadastro de beneficiario

---
+++ src/Cadastro de Beneficiario/cadastro_beneficiario/cadastro_beneficiario/Controllers/BeneficiariosController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using cadastro_beneficiario.Models;
+
+namespace cadastro_beneficiario.Controllers
+{
+    public class BeneficiariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BeneficiariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Beneficiarios
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Beneficiarios.ToListAsync());
+        }
+
+        // GET: Beneficiarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Beneficiarios == null)
+            {
+                return NotFound();
+            }
+
+            var beneficiario = await _context.Beneficiarios
+                .FirstOrDefaultAsync(m => m.Id_Beneficiario == id);
+            if (beneficiario == null)
+            {
+                return NotFound();
+            }
+
+            return View(beneficiario);
+        }
+
+        // GET: Beneficiarios/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Beneficiarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id_Beneficiario,CPF,Nome_Beneficiario,Situacao,Data_Nascimento,CEP,Logradouro,Numero,Complemento,Cidade,UF")] Beneficiario beneficiario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(beneficiario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(beneficiario);
+        }
+
+        // GET: Beneficiarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Beneficiarios == null)
+            {
+                return NotFound();
+            }
+
+            var beneficiario = await _context.Beneficiarios.FindAsync(id);
+            if (beneficiario == null)
+            {
+                return NotFound();
+            }
+            return View(beneficiario);
+        }
+
+        // POST: Beneficiarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id_Beneficiario,CPF,Nome_Beneficiario,Situacao,Data_Nascimento,CEP,Logradouro,Numero,Complemento,Cidade,UF")] Beneficiario beneficiario)
+        {
+            if (id != beneficiario.Id_Beneficiario)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(beneficiario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BeneficiarioExists(beneficiario.Id_Beneficiario))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(beneficiario);
+        }
+
+        // GET: Beneficiarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Beneficiarios == null)
+            {
+                return NotFound();
+            }
+
+            var beneficiario = await _context.Beneficiarios
+                .FirstOrDefaultAsync(m => m.Id_Beneficiario == id);
+            if (beneficiario == null)
+            {
+                return NotFound();
+            }
+
+            return View(beneficiario);
+        }
+
+        // POST: Beneficiarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Beneficiarios == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Beneficiarios'  is null.");
+            }
+            var beneficiario = await _context.Beneficiarios.FindAsync(id);
+            if (beneficiario != null)
+            {
+                _context.Beneficiarios.Remove(beneficiario);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BeneficiarioExists(int id)
+        {
+          return _context.Beneficiarios.Any(e => e.Id_Beneficiario == id);
+        }
+    }
+}
