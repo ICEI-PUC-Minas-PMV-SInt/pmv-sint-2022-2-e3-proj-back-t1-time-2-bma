7b0968ec7a3c4f74813770ffe8ffa1a424694b79
dayanabeirigo <49229699+dayanabeirigo@users.noreply.github.com>
2022-11-30 16:47:32 -0300

    Registro de doações 00

---
+++ src/Dayana/banco-alimentos/banco-alimentos/Migrations/20221130021801_M00.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace banco_alimentos.Migrations
+{
+    public partial class M00 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Beneficiarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CPF = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Beneficiarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Beneficiarios");
+        }
+    }
+}
---
+++ src/Dayana/banco-alimentos/banco-alimentos/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using banco_alimentos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_web_backend.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Beneficiario> Beneficiarios { get; set; }
+        public DbSet<Doacao> Doacoes { get; set; }
+
+    }
+}
